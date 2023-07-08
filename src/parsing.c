b0482cf6cef14620a1dc7fa872311af34ff29230
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-07-08 12:15:42 +0530

    User input evaluation
    Addition of two extra operators:
        - modulo (%)
        - exponent (^)
    Also make addition and subtraction apply to single inputs

--- src/parsing.c
+++ src/parsing.c
@@ -1,9 +1,14 @@
+#include <editline/readline.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../lib/mpc/mpc.c"
-#include <editline/readline.h>
+
+long eval (mpc_ast_t*);
+long eval_op (long, char*, long);
 
 int
 main (int argc, char** argv)
@@ -17,14 +22,12 @@ main (int argc, char** argv)
     mpca_lang (MPCA_LANG_DEFAULT,
         "                                                       \
             number   : /-?[0-9]+/ ;                             \
-            operator : '+' | '-' | '*' | '/' ;                  \
+            operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
             expr     : <number> | '(' <operator> <expr>+ ')' ;  \
             toosty   : /^/ <operator> <expr>+ /$/ ;             \
         ",
         Number, Operator, Expr, Toosty);
     // clang-format on
-
-    puts ("Toosty version 0.0.1");
 
     while (true)
         {
@@ -32,9 +35,18 @@ main (int argc, char** argv)
             add_history (ibuffer);
 
             mpc_result_t r;
+
+            if (strcmp (ibuffer, "exit") == 0)
+                {
+                    free (ibuffer);
+                    break;
+                }
+
             if (mpc_parse ("<stdin>", ibuffer, Toosty, &r))
                 {
-                    mpc_ast_print (r.output);
+                    // mpc_ast_print (r.output);
+                    long result = eval (r.output);
+                    printf ("%li\n", result);
                     mpc_ast_delete (r.output);
                 }
             else
@@ -49,4 +61,38 @@ main (int argc, char** argv)
     mpc_cleanup (4, Number, Operator, Expr, Toosty);
 
     return EXIT_SUCCESS;
+}
+
+long
+eval (mpc_ast_t* t)
+{
+    if (strstr (t->tag, "number")) return strtol (t->contents, NULL, 10);
+
+    char* op = t->children[1]->contents;
+
+    long x = eval (t->children[2]);
+
+    size_t i = 3;
+    while (strstr (t->children[i]->tag, "expr"))
+        {
+            x = eval_op (x, op, eval (t->children[i]));
+            i++;
+        }
+
+    if (!(i ^ 3) && strcmp (op, "-") == 0) return --x;
+    if (!(i ^ 3) && strcmp (op, "+") == 0) return ++x;
+
+    return x;
+}
+
+long
+eval_op (long x, char* op, long y)
+{
+    if (strcmp (op, "+") == 0) { return x + y; }
+    if (strcmp (op, "-") == 0) { return x - y; }
+    if (strcmp (op, "*") == 0) { return x * y; }
+    if (strcmp (op, "/") == 0) { return x / y; }
+    if (strcmp (op, "%") == 0) { return x % y; }
+    if (strcmp (op, "^") == 0) { return pow (x, y); }
+    return 0;
 }
