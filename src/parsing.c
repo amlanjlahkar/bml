4ec03862b368e06ad388d5366cb3a5a39e802034
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-07-10 23:03:27 +0530

    Error handling
    Values produced during input evaluation now contain some definite properties which are held by the 'tval' struct:
        - type: evaluated result type
        - err: resultant error(0 by default meaning no error)
        - val: resultant value
    
    The two functions 'tval_res()' and 'tval_err()' are used for returning
    corresponding tval objects for either successful or defective evaluation
    
    For convenience, two enums are used to store the different types for the 'type' and the 'err' member:
        - tval_type_res (number(0) or error(1))
        - tval_type_err (divide_by_zero(1), bad_operation(2), bad_operand(3))

--- src/parsing.c
+++ src/parsing.c
@@ -1,4 +1,5 @@
 #include <editline/readline.h>
+#include <errno.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -7,16 +8,26 @@
 
 #include "../lib/mpc/mpc.c"
 
-long eval (mpc_ast_t*);
-long eval_op (long, char*, long);
+typedef struct {
+    int type;
+    long val;
+    int err;
+} tval;
 
-int
-main (int argc, char** argv)
-{
-    mpc_parser_t* Number   = mpc_new ("number");
-    mpc_parser_t* Operator = mpc_new ("operator");
-    mpc_parser_t* Expr     = mpc_new ("expr");
-    mpc_parser_t* Toosty   = mpc_new ("toosty");
+enum tval_type_res { TVAL_NUM, TVAL_ERR };
+enum tval_type_err { TERR_DIV_ZERO = 1, TERR_BAD_OP = 2, TERR_BAD_NUM = 3 };
+
+tval eval(mpc_ast_t*);
+tval eval_op(tval, char* op, tval);
+tval tval_res(long);
+tval tval_err(int);
+void tval_print(tval);
+
+int main(int argc, char** argv) {
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Toosty   = mpc_new("toosty");
 
     // clang-format off
     mpca_lang (MPCA_LANG_DEFAULT,
@@ -29,73 +40,116 @@ main (int argc, char** argv)
         Number, Operator, Expr, Toosty);
     // clang-format on
 
-    while (true)
-        {
-            char* ibuffer = readline ("toosty> ");
-            add_history (ibuffer);
+    while (true) {
+        char* ibuffer = readline("toosty> ");
+        add_history(ibuffer);
 
-            mpc_result_t r;
+        mpc_result_t r;
 
-            if (strcmp (ibuffer, "exit") == 0)
-                {
-                    free (ibuffer);
-                    break;
-                }
-
-            if (mpc_parse ("<stdin>", ibuffer, Toosty, &r))
-                {
-                    // mpc_ast_print (r.output);
-                    long result = eval (r.output);
-                    printf ("%li\n", result);
-                    mpc_ast_delete (r.output);
-                }
-            else
-                {
-                    mpc_err_print (r.error);
-                    mpc_err_delete (r.error);
-                }
-
-            free (ibuffer);
+        if (strcmp(ibuffer, "exit") == 0) {
+            free(ibuffer);
+            break;
         }
 
-    mpc_cleanup (4, Number, Operator, Expr, Toosty);
+        if (mpc_parse("<stdin>", ibuffer, Toosty, &r)) {
+            void* ast = r.output;
+            // mpc_ast_print(ast);
+            tval result = eval(ast);
+            tval_print(result);
+            mpc_ast_delete(ast);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(ibuffer);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Toosty);
 
     return EXIT_SUCCESS;
 }
 
-long
-eval (mpc_ast_t* t)
-{
-    if (strstr (t->tag, "number")) return strtol (t->contents, NULL, 10);
+tval eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        errno  = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? tval_res(x) : tval_err(TERR_BAD_NUM);
+    }
 
     char* op = t->children[1]->contents;
 
-    long x = eval (t->children[2]);
+    tval x = eval(t->children[2]);
 
     size_t i = 3;
-    while (strstr (t->children[i]->tag, "expr"))
-        {
-            x = eval_op (x, op, eval (t->children[i]));
-            i++;
-        }
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
 
-    if (!(i ^ 3) && strcmp (op, "-") == 0) return --x;
-    if (!(i ^ 3) && strcmp (op, "+") == 0) return ++x;
+    if (!(i ^ 3) && strcmp(op, "-") == 0) x.val -= 1;
+    if (!(i ^ 3) && strcmp(op, "+") == 0) x.val += 1;
 
     return x;
 }
 
-long
-eval_op (long x, char* op, long y)
-{
-    if (strcmp (op, "+") == 0) { return x + y; }
-    if (strcmp (op, "-") == 0) { return x - y; }
-    if (strcmp (op, "*") == 0) { return x * y; }
-    if (strcmp (op, "/") == 0) { return x / y; }
-    if (strcmp (op, "%") == 0) { return x % y; }
-    if (strcmp (op, "^") == 0) { return pow (x, y); }
-    if (strcmp (op, "min") == 0) { return (x < y) ? x : y; }
-    if (strcmp (op, "max") == 0) { return (x > y) ? x : y; }
+tval eval_op(tval x, char* op, tval y) {
+    if (x.type == TVAL_ERR) return x;
+    if (y.type == TVAL_ERR) return y;
 
-    return 0;
+    if (strcmp(op, "+") == 0) return tval_res(x.val + y.val);
+    if (strcmp(op, "-") == 0) return tval_res(x.val - y.val);
+    if (strcmp(op, "*") == 0) return tval_res(x.val * y.val);
+    if (strcmp(op, "^") == 0) return tval_res(pow(x.val, y.val));
+
+    if (strcmp(op, "/") == 0)
+        return y.val == 0 ? tval_err(TERR_DIV_ZERO) : tval_res(x.val / y.val);
+
+    if (strcmp(op, "%") == 0)
+        return y.val == 0 ? tval_err(TERR_DIV_ZERO) : tval_res(x.val % y.val);
+
+    if (strcmp(op, "min") == 0)
+        return tval_res((x.val < y.val) ? x.val : y.val);
+
+    if (strcmp(op, "max") == 0)
+        return tval_res((x.val > y.val) ? x.val : y.val);
+
+    return tval_err(TERR_BAD_OP);
+}
+
+tval tval_res(long result) {
+    tval v;
+    v.type = TVAL_NUM;
+    v.val  = result;
+    v.err = 0;
+    return v;
+}
+
+tval tval_err(int err) {
+    tval v;
+    v.type = TVAL_ERR;
+    v.err  = err;
+    return v;
+}
+
+void tval_print(tval v) {
+    switch (v.type) {
+        case TVAL_NUM:
+            printf("%li\n", v.val);
+            break;
+
+        case TVAL_ERR:
+            switch (v.err) {
+                case TERR_DIV_ZERO:
+                    puts("Error: Attempted division by zero!");
+                    break;
+                case TERR_BAD_OP:
+                    puts("Error: Invalid operator!");
+                    break;
+                case TERR_BAD_NUM:
+                    puts("Error: Invalid number!");
+                    break;
+            }
+            break;
+    }
 }
